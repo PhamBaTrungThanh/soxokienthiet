edb1894dcf9baeb645e2f780fb3f94f7cf39a9f0
Arthur <phambatrungthanh@gmail.com>
2019-10-25 01:43:46 +0000

    Test redis

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/cache', function () use ($router) {
+    app('redis')->set('ping', 'PONG');
+    return app('redis')->get('ping');
+});
