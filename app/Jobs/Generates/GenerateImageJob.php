1a6f91961d22d69fc568180092dc5f0991f9ffdc
Arthur <phambatrungthanh@gmail.com>
2020-02-18 10:52:53 +0000

    Move generate map to image job

--- app/Jobs/Generates/GenerateImageJob.php
+++ app/Jobs/Generates/GenerateImageJob.php
@@ -3,6 +3,8 @@
 namespace App\Jobs\Generates;
 
 use App\Jobs\Job;
+use Exception;
+use Illuminate\Support\Arr;
 
 class GenerateImageJob extends Job
 {
@@ -14,10 +16,9 @@ class GenerateImageJob extends Job
     public $cellSize;
     public $fileName;
 
-    public function __construct(string $fileName, array $map)
+    public function __construct(string $fileName)
     {
         $this->fileName = $fileName;
-        $this->map = $map;
         $this->cellSize = config('app.image.cell_size');
     }
 
@@ -26,19 +27,35 @@ class GenerateImageJob extends Job
      */
     public function handle()
     {
+        $key = sprintf('%s:%s', config('app.lottery.key'), $this->date);
+        $storedData = app('redis')->get($key);
+        if ('' === trim($storedData)) {
+            throw new Exception('No data found for key: '.$key);
+        }
+        $data = $this->sanitizeData($storedData);
+        $jackpot = $this->getJackpot($storedData);
+
+        $this->map = $this->generateMap($data, $jackpot);
+
+        $this->makeImage();
+    }
+
+    private function makeImage()
+    {
         $imageDimension = $this->cellSize * self::MATRIX_DIMENSION;
 
         $image = ImageCreate($imageDimension, $imageDimension);
         $colorWhite = ImageColorAllocate($image, 0xFF, 0xFF, 0xFF);
         $colorBlack = ImageColorAllocate($image, 0x00, 0x00, 0x00);
+        $colorJackpot = ImageColorAllocate($image, 0xFF, 0x00, 0x00);
 
         foreach ($this->map as $rowIndex => $row) {
             $top = $rowIndex * $this->cellSize;
             foreach ($row as $cellIndex => $cell) {
                 $left = $cellIndex * $this->cellSize;
-                if (1 === $cell) {
+                if (filled($cell)) {
                     // fill
-                    ImageFilledRectangle($image, $left, $top, $left + $this->cellSize, $top + $this->cellSize, $colorBlack);
+                    ImageFilledRectangle($image, $left, $top, $left + $this->cellSize, $top + $this->cellSize, 1 === $cell ? $colorBlack : $colorJackpot);
                 }
             }
         }
@@ -46,7 +63,48 @@ class GenerateImageJob extends Job
         imagepng($image, storage_path("images/single/{$this->fileName}.png"));
 
         imagedestroy($image);
+    }
 
-        dispatch(new GenerateImageGridJob($this->fileName));
+    private function sanitizeData($data)
+    {
+        $data = json_decode($data);
+
+        $flatten = collect(Arr::flatten($data));
+
+        return $flatten->map(function ($item) {
+            return substr($item, -2);
+        });
+    }
+
+    private function getJackpot($data)
+    {
+        $data = json_decode($data);
+
+        return substr(data_get($data, 'jackpot'), -2);
+    }
+
+    private function generateMap($sanitizedData, $jackpot)
+    {
+        $map = [];
+        // generate empty matrix
+        for ($row = 0; $row < self::MATRIX_DIMENSION; ++$row) {
+            $map[$row] = [];
+            for ($col = 0; $col < self::MATRIX_DIMENSION; ++$col) {
+                $map[$row][$col] = 0;
+            }
+        }
+
+        // loop through data and make a dot for each value
+        foreach ($sanitizedData as $dimension) {
+            $row = $dimension[0];
+            $col = $dimension[1];
+            if ($jackpot === $row.$col) {
+                $map[$row][$col] = 2;
+            } else {
+                $map[$row][$col] = 1;
+            }
+        }
+
+        return $map;
     }
 }
